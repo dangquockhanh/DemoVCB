29ca316b6121d38fb3fb367893962b6d76045776
Khánh Trắng <harry.edward456@gmail.com>
2019-05-29 08:21:43 +0700

    image Header

---
+++ DemoVietComBank/DemoVietComBank/MenuCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCollectionViewCell.swift
+//  DemoVietComBank
+//
+//  Created by Đặng Khánh  on 5/28/19.
+//  Copyright © 2019 Khánh Trắng. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var menuCell: UIImageView!
+    
+}
